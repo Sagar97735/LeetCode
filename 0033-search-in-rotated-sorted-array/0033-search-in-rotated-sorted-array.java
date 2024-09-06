1bb71ffaedef850c7799fdcc2022e9dd2ca490db
Sagar97735 <sagarrajsingh370@gmail.com>
2024-09-06 19:37:37 +0530

    Time: 0 ms (100%), Space: 42.1 MB (29.83%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,45 +1,39 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int index = 0;
-        int a = -1;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i]==target){
-              return i;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            // Check if the target is at the mid index
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Determine which part is sorted
+            if (nums[start] <= nums[mid]) {
+                // Left part is sorted
+                if (target >= nums[start] && target < nums[mid]) {
+                    // Target is in the left part
+                    end = mid - 1;
+                } else {
+                    // Target is in the right part
+                    start = mid + 1;
+                }
+            } else {
+                // Right part is sorted
+                if (target > nums[mid] && target <= nums[end]) {
+                    // Target is in the right part
+                    start = mid + 1;
+                } else {
+                    // Target is in the left part
+                    end = mid - 1;
+                }
             }
         }
-    // int start1 = 0;
-    // int end1 = 0;
-    // int end = nums.length-1;
-    // int start = 0;
-    //  if(nums[nums.length-1]!=nums[index]){
-    //     start1 = index+1;
-    //     end1 = nums.length-1;
-    //     start =0;
-    //     end = index-1;
-    //  }
-      
-    //    while(start<=end || start1<=end1){
-    //      int mid1 = start + (end - start) / 2;
-    //      int mid2 = start1 + (end1-start1)/2;
-    //     if(nums[mid1]<target){
-    //       start = mid1+1;
-    //     }
-    //      if(nums[mid1]>target){
-    //       end = mid1-1;
-    //     }
-    //      if(nums[mid2]<target){
-    //       start1 = mid2+1;
-    //     }
-    //     else if(nums[mid2]>target){
-    //       end1 = mid2-1;
-    //     }
-    //    if(nums[mid1]==target){
-    //     return mid1;
-    //    }
-    //    if(nums[mid2]==target){
-    //     return mid2;
-    //    }
-    //    } 
-       return -1;
+
+        // Target not found
+        return -1;
     }
 }
