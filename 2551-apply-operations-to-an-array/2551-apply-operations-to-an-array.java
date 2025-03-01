9c0560232c70550e0598bf30507859f1b2fbcf6f
Sagar97735 <sagarrajsingh370@gmail.com>
2025-03-02 01:50:34 +0530

    Time: 1 ms (96.52%), Space: 43.5 MB (92.58%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        for(int i =0; i< nums.length-1; i++){
+        for(int i =0; i<nums.length-1; i++){
             if(nums[i]==nums[i+1]){
                 nums[i] = 2*nums[i];
                 nums[i+1]=0;
             }
         }
-        int[] arr = new int[nums.length];
-        int j =0;
-        for(int i =0; i<nums.length; i++){
-          if(nums[i]!=0){
-            arr[j]=nums[i];
-            j++;
-          }
-        }
-        return arr;
+        int i = 0;
+    for(int n: nums){                     // collect all noon zero values at front
+        if(n != 0) nums[i++] = n;
     }
+    while(i < nums.length) nums[i++] = 0;  //set values to zero for remainintg ith element
+    return nums;
+}
 }
