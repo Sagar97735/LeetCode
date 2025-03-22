f62ba945d5a7397db97d928fbf05b5d879bdfa81
Sagar97735 <sagarrajsingh370@gmail.com>
2025-03-22 20:29:41 +0530

    Time: 1 ms (92.83%), Space: 44.8 MB (31.26%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public static void solve(List<List<Integer>> arr, List<Integer> arr1, int[] nums, int idx, boolean[] used) {
+        if (arr1.size() == nums.length) {
+            arr.add(new ArrayList<>(arr1));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) { // Iterate from 0 to consider all numbers
+            if (!used[i]) { // Ensure each number is used only once per permutation
+                used[i] = true;
+                arr1.add(nums[i]);
+
+                solve(arr, arr1, nums, i, used); // Recursive call
+
+                // Backtrack
+                arr1.remove(arr1.size() - 1);
+                used[i] = false;
+            }
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> arr1 = new ArrayList<>();
+        List<List<Integer>> arr = new ArrayList<>();
+        boolean[] used = new boolean[nums.length]; // Track used numbers
+
+        solve(arr, arr1, nums, 0, used);
+        return arr;
+    }
+}
