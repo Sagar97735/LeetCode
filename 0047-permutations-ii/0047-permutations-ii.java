1a411f4309acb374acca36a1aac6888b9aafe799
Sagar97735 <sagarrajsingh370@gmail.com>
2025-03-26 23:38:03 +0530

    Time: 32 ms (20.05%), Space: 45.1 MB (38.39%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,27 +1,28 @@
-public class Solution {
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        if(nums==null || nums.length==0) return res;
-        boolean[] used = new boolean[nums.length];
-        List<Integer> list = new ArrayList<Integer>();
-        Arrays.sort(nums);
-        dfs(nums, used, list, res);
-        return res;
-    }
-
-    public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res){
-        if(list.size()==nums.length){
-            res.add(new ArrayList<Integer>(list));
+class Solution {
+    private HashSet<List<Integer>> result = new HashSet<>();
+    private int n;
+    
+    public void solve(int idx, List<Integer> nums) {
+        if (idx == n) {
+            result.add(new ArrayList<>(nums));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(used[i]) continue;
-            if(i>0 &&nums[i-1]==nums[i] && !used[i-1]) continue;
-            used[i]=true;
-            list.add(nums[i]);
-            dfs(nums,used,list,res);
-            used[i]=false;
-            list.remove(list.size()-1);
+        
+        for (int i = idx; i < n; i++) {
+            Collections.swap(nums, i, idx);
+            solve(idx + 1, nums);
+            Collections.swap(nums, i, idx);
         }
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+          n = nums.length;
+        List<Integer> numList = new ArrayList<>();
+        for (int num : nums) {
+            numList.add(num);
+        }
+        
+        solve(0, numList);
+        List<List<Integer>> ans = new ArrayList<>(result);
+        return ans;
     }
 }
