2450d91e97506385ac65a48fa9148a8820511b94
Sagar97735 <sagarrajsingh370@gmail.com>
2025-03-30 22:39:12 +0530

    Time: 4 ms (91.22%), Space: 45.5 MB (57.3%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,41 +1,36 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-        for(int i =0; i< numCourses; i++){
-            arr.add(new ArrayList<>());
-        }
-        for(int i =0; i< prerequisites.length; i++){
-         int a = prerequisites[i][0];
-         int b = prerequisites[i][1];
-         arr.get(b).add(a);              
-        }
-       int [] indegree = new int[numCourses];
-       for(int i =0; i< arr.size(); i++){
-        for(int j =0; j<arr.get(i).size(); j++){
-            indegree[arr.get(i).get(j)]++;
-        }
-       }
-       Queue<Integer> q1 = new LinkedList<>();
-       for(int i =0; i<indegree.length; i++){
-        if(indegree[i]==0){
-            q1.add(i);
-        }
-       }
-       int count = 0;
-       while(!q1.isEmpty()){
-        int a = q1.poll();
-        count++;
-        for(int i = 0; i<arr.get(a).size(); i++){
-            int neigh = arr.get(a).get(i);
-            indegree[neigh]--;
-            if(indegree[neigh]==0){
-                q1.add(neigh);
+    public static boolean dfs(int i, boolean[] stack,boolean [] vis , ArrayList<ArrayList<Integer>> arr){
+        vis[i]   = true;
+        stack[i] = true;
+        for(int neighbour : arr.get(i)){
+            if(!vis[neighbour]){
+               if(dfs(neighbour,stack,vis,arr)){
+                return true;
+               }
+            }
+            else if(stack[neighbour]){
+                return true;
             }
         }
-       }
-       if(numCourses == count){
-        return true;
-       }
-       return false;
+        stack[i] = false;
+        return false;
     }
-}
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+       ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+       for(int j=0; j<numCourses; j++){
+        arr.add(new ArrayList<>());
+       }
+        for(int i=0; i<prerequisites.length; i++){
+          arr.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+      boolean [] stack   = new boolean[numCourses];
+      boolean [] vis = new boolean[numCourses];
+      for(int i = 0; i<arr.size(); i++){
+             if(!vis[i]){
+              if(dfs(i,stack,vis,arr)){
+                return false;
+              }          
+             }          
+      }
+      return true;
+    }}
