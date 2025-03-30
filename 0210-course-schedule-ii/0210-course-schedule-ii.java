b3a900a8ab6cd97e1b9c46da64456f0fb8d7889c
Sagar97735 <sagarrajsingh370@gmail.com>
2025-03-30 23:30:38 +0530

    Time: 5 ms (79.09%), Space: 46.4 MB (18.64%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,48 +1,45 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-      ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-      for(int i =0;i< numCourses; i++){
+         ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+       for(int j=0; j<numCourses; j++){
         arr.add(new ArrayList<>());
-      } 
-       
-       for(int i =0; i<prerequisites.length; i++){
-        int a = prerequisites[i][0];
-        int b = prerequisites[i][1];
-        arr.get(b).add(a);
-       } 
-       int [] indegree = new int[numCourses];
-       for(int i =0; i<arr.size(); i++){
-        for(int j =0; j<arr.get(i).size(); j++){
-            int neigh = arr.get(i).get(j);
-            indegree[neigh]++;
-        }
        }
-       Queue<Integer> q1 = new LinkedList<>();
-       for(int i =0; i< indegree.length; i++){
+        for(int i=0; i<prerequisites.length; i++){
+          arr.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        int [] indegree = new int[numCourses];
+        Queue<Integer> q1 = new LinkedList<>();
+        for(int i =0; i<prerequisites.length; i++){
+           int a = prerequisites[i][0];
+           int b = prerequisites[i][1];
+           indegree[a]++;
+        }
+       for(int i = 0; i<indegree.length; i++){
         if(indegree[i]==0){
             q1.add(i);
         }
        }
-       int [] ans = new int[numCourses];
-       int [] a1 = new int[1];
-       int m = 0;
-       int count = 0;
+       int cnt = 0;
+       ArrayList<Integer> arr1 = new ArrayList<>();
        while(!q1.isEmpty()){
-        int a = q1.poll();
-        ans[m] = a;
-        m++;
-        count++;
-        for(int i =0; i<arr.get(a).size(); i++){
-            int neigh = arr.get(a).get(i);
-            indegree[neigh]--;
-            if(indegree[neigh]==0){
-                q1.add(neigh);
+         int curr = q1.poll();
+         arr1.add(curr);
+         cnt++;
+         for(int i = 0; i<arr.get(curr).size(); i++){
+            int a = arr.get(curr).get(i);
+            indegree[a]--;
+            if(indegree[a]==0){
+                q1.add(a);
             }
-        }
+         }
        }
-       if(count == numCourses){
-        return ans;
+       int [] ans = new int[numCourses];
+       for(int i= 0; i< arr1.size(); i++){
+           ans[i] = arr1.get(i);
        }
-       return new int[0];
+       if(cnt!=numCourses){
+        return new int[0];
+       }
+       return ans;
     }
 }
