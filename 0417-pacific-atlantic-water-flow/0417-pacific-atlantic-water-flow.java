9acd5953a9718b646692b9ed98081feb1e1a9972
Sagar97735 <sagarrajsingh370@gmail.com>
2025-03-31 20:26:55 +0530

    Time: 303 ms (6.86%), Space: 46.5 MB (5.82%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,73 +1,31 @@
 class Solution {
-    public boolean isSolve1(int[][] heights, int i, int j, boolean[][] visited) {
-        if (i == 0 || j == 0) {
-            return true;
-        }
-        if (visited[i][j]) {
-            return false;
-        }
-        visited[i][j] = true;
-        
-        boolean res = false;
-        if (i - 1 >= 0 && heights[i - 1][j] <= heights[i][j]) {
-            res |= isSolve1(heights, i - 1, j, visited);
-        }
-        if (j - 1 >= 0 && heights[i][j - 1] <= heights[i][j]) {
-            res |= isSolve1(heights, i, j - 1, visited);
-        }
-        if (i + 1 < heights.length && heights[i + 1][j] <= heights[i][j]) {
-            res |= isSolve1(heights, i + 1, j, visited);
-        }
-        if (j + 1 < heights[0].length && heights[i][j + 1] <= heights[i][j]) {
-            res |= isSolve1(heights, i, j + 1, visited);
-        }
-        
-        return res;
-    }
-    
-    public boolean isSolve2(int[][] heights, int i, int j, boolean[][] visited) {
-        if (i == heights.length - 1 || j == heights[0].length - 1) {
-            return true;
-        }
-        if (visited[i][j]) {
-            return false;
-        }
-        visited[i][j] = true;
-        
-        boolean res = false;
-        if (i + 1 < heights.length && heights[i + 1][j] <= heights[i][j]) {
-            res |= isSolve2(heights, i + 1, j, visited);
-        }
-        if (j + 1 < heights[0].length && heights[i][j + 1] <= heights[i][j]) {
-            res |= isSolve2(heights, i, j + 1, visited);
-        }
-        if (i - 1 >= 0 && heights[i - 1][j] <= heights[i][j]) {
-            res |= isSolve2(heights, i - 1, j, visited);
-        }
-        if (j - 1 >= 0 && heights[i][j - 1] <= heights[i][j]) {
-            res |= isSolve2(heights, i, j - 1, visited);
-        }
-        
-        return res;
-    }
-    
+    List<List<Integer>> res;
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int m = heights.length, n = heights[0].length;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                boolean[][] pacificVisited = new boolean[m][n];
-                boolean[][] atlanticVisited = new boolean[m][n];
-                
-                boolean ans1 = isSolve1(heights, i, j, pacificVisited);
-                boolean ans2 = isSolve2(heights, i, j, atlanticVisited);
-                
-                if (ans1 && ans2) {
-                    ans.add(Arrays.asList(i, j));
+        res = new ArrayList<>();
+        for(int i = 0;i<heights.length;i++){
+            for(int j=0;j<heights[0].length;j++){
+                if((i==heights.length-1 && j==0) || (i==0 && j==heights[0].length-1)){
+                    res.add(Arrays.asList(i,j));
+                    continue;
                 }
+                boolean[] pair = new boolean[2];
+                boolean[][] visited = new boolean[heights.length][heights[0].length];
+                dfs(heights,i,j,pair,visited,heights[i][j]);
+                if(pair[0] && pair[1]) res.add(Arrays.asList(i,j));
             }
         }
-        return ans;
+        return res;
+    }
+    public void dfs(int[][] heights,int i,int j,boolean[] pair,boolean[][] visited,int prev){
+        if(i<0 || j<0 || i>=heights.length || j>=heights[0].length || visited[i][j] == true || heights[i][j] > prev) return;
+        if(i == 0 || j==0) pair[0] = true;
+        if(i == heights.length-1 || j == heights[0].length-1) pair[1] = true;
+        if(pair[0] && pair[1]) return;
+        prev = heights[i][j];
+        visited[i][j] = true;
+        dfs(heights,i-1,j,pair,visited,prev);
+        dfs(heights,i,j+1,pair,visited,prev);
+        dfs(heights,i+1,j,pair,visited,prev);
+        dfs(heights,i,j-1,pair,visited,prev);
     }
 }
