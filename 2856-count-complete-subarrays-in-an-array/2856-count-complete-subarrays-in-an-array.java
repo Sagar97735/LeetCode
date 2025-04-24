182c91d95d4cce745f05014be94de7f735330b3a
Sagar97735 <sagarrajsingh370@gmail.com>
2025-04-24 10:15:03 +0530

    Time: 6 ms (95.49%), Space: 45.4 MB (23.52%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -1,40 +1,25 @@
 class Solution {
-    public int solve(int [] nums,int idx){
-        HashMap<Integer,Integer>h1 = new HashMap<>();
-        for(int i =0; i<nums.length; i++){
-            h1.put(nums[i],0);
+    public int countCompleteSubarrays(int[] nums) {
+        HashSet<Integer> h1 = new HashSet<>();
+        HashMap<Integer,Integer> h2 = new HashMap<>();
+        for(int i =0;i<nums.length; i++){
+            h1.add(nums[i]);
         }
-        int size = h1.size();
-        int cnt =0;
+        int i = 0;
+        int j = 0;
         int ans = 0;
-        for(int i =idx; i<nums.length; i++){
-            int curr = nums[i];
-        if(h1.get(curr)==1){
-            continue;
-          }
-        else if(h1.get(curr)==0){
-            h1.put(curr,1);
-            cnt++;
-          }
-        
-        if(cnt==size){
-          ans = nums.length-i;
-          return ans;
-        }
+        while(j<nums.length){
+         h2.put(nums[j],h2.getOrDefault(nums[j],0)+1);
+         while(h2.size()==h1.size()){
+          ans+=(nums.length-j);
+          h2.put(nums[i],h2.get(nums[i])-1);
+           if (h2.get(nums[i]) == 0) {
+                    h2.remove(nums[i]);
+                }
+                i++;
+         }
+         j++;
         }
         return ans;
-    }
-    public int countCompleteSubarrays(int[] nums) {
-         int ans = 0;
-         for(int i =0; i<nums.length; i++){
-            int ans1 = solve(nums,i);
-            if(ans1==0){
-                break;
-            }
-            else{
-                ans+=ans1;
-            }
-         }
-         return ans;
     }
 }
