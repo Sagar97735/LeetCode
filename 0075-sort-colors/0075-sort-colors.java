a665c142eafbcf4590fd5ffd62605491852e585a
Sagar97735 <sagarrajsingh370@gmail.com>
2025-05-17 20:24:31 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.4%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,33 +1,63 @@
+// ------------------------->approach1<---------------------------------
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int cnt0 = 0;
+//         int cnt1 = 0;
+//         int cnt2 = 0;
+//     for(int i = 0; i<nums.length; i++){
+//         if(nums[i]==0){
+//             cnt0++;
+//         }
+//         if(nums[i]==1){
+//             cnt1++;
+//         }
+//         if(nums[i]==2){
+//             cnt2++;
+//         }
+//     }
+//     for(int i =0; i<nums.length; i++){
+//         if(cnt0!=0){
+//             nums[i]=0;
+//             cnt0--;
+//         }
+//         else if(cnt1!=0){
+//             nums[i]=1;
+//             cnt1--;
+//         }
+//         else if(cnt2!=0){
+//              nums[i]=2;
+//              cnt2--;
+//         }
+//     }
+
+//     }
+// }
+// ------------------------->Approach2<----------------------------------
 class Solution {
     public void sortColors(int[] nums) {
-        int cnt0 = 0;
-        int cnt1 = 0;
-        int cnt2 = 0;
-    for(int i = 0; i<nums.length; i++){
-        if(nums[i]==0){
-            cnt0++;
+        int n = nums.length;
+
+        int i = 0;   // denotes for 0
+        int j = 0;   // denotes for 1
+        int k = n - 1; // denotes for 2
+
+        while (j <= k) {
+            if (nums[j] == 1) {
+                j++;
+            } else if (nums[j] == 2) {
+                //swap with nums[k]
+                int temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
+                k--;
+            } else { // if nums[j] == 0
+             //swap with nums[i]
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+                j++;
+            }
         }
-        if(nums[i]==1){
-            cnt1++;
-        }
-        if(nums[i]==2){
-            cnt2++;
-        }
-    }
-    for(int i =0; i<nums.length; i++){
-        if(cnt0!=0){
-            nums[i]=0;
-            cnt0--;
-        }
-        else if(cnt1!=0){
-            nums[i]=1;
-            cnt1--;
-        }
-        else if(cnt2!=0){
-             nums[i]=2;
-             cnt2--;
-        }
-    }
-    
     }
 }
