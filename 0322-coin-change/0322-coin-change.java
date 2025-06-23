03b48dc0a6caa3a42c529c76e75702424a740605
Sagar97735 <sagarrajsingh370@gmail.com>
2025-06-23 21:15:59 +0530

    Time: 22 ms (37.59%), Space: 45.1 MB (34.4%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,35 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[][] dp = new int [n+1][amount+1];  // making dp array with size of coins array and amount
+    public void isSolve(int [] coins, int [][] dp){
         
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = 0;   // When amount is 0 then 0 coins are required 
-        }
-        for(int i = 0; i <= amount ; i++){
-            dp[0][i] = Integer.MAX_VALUE - 1; //When there is 0 coins then amount can't be made 
-        }
-        
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= amount; j++){
-                if(coins[i-1] <= j){ // if coin at i-1th position is less than or equal to amount
-                    dp[i][j] = Math.min(dp[i-1][j], 1+dp[i][j - coins[i-1]]);  
+      for(int i =1; i<dp.length; i++){
+        for(int j =1; j<dp[0].length; j++){
+            if(coins[i-1]<=j){
+                      dp[i][j] = Math.min(dp[i-1][j],1+ dp[i][j-coins[i-1]]);
                 }
-                else
-                    dp[i][j] = dp[i-1][j]; 
+            
+            else{
+                dp[i][j] = dp[i-1][j];
             }
         }
-        return dp[n][amount] != Integer.MAX_VALUE - 1 ? dp[n][amount] : -1;
-    }}
+      }
+      return;
+    }
+    public int coinChange(int[] coins, int amount) {
+        if(amount ==0){
+            return 0;
+        }
+        int n = coins.length;
+        int [][] dp = new int[coins.length+1][amount+1];
+        Arrays.sort(coins);
+        for (int i = 0; i <= coins.length; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (j == 0) dp[i][j] = 0; // 0 coins to make 0 amount
+                else dp[i][j] = Integer.MAX_VALUE - 1;
+            }
+        }
+
+       
+        isSolve(coins,dp);
+         return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
+    }
+}
