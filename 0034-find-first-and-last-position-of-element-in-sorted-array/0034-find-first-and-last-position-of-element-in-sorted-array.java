7eb44e0e1e1aa35aa7d0ed64f932dda61bc404a8
Sagar97735 <sagarrajsingh370@gmail.com>
2025-06-28 17:03:08 +0530

    Time: 0 ms (100%), Space: 45.9 MB (83.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,34 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int [] ans = new int[2];
-        
-        ans[0] =-1;
-        ans[1] = -1;
+        Arrays.fill(ans,-1);
         int start = 0;
+        int i = 0;
+        int j = 0;
         int end = nums.length-1;
-        int i = -1;
-        int j = -1;
+        int mid = (start + end)/2;
         while(start<=end){
-             int mid = start + (end-start)/2 ;
-             if(mid>=0 && nums[mid]>target){
+            mid = (start + end)/2;
+            if(nums[mid]<target){
+               start = mid+1;
+            }
+            else if(nums[mid]>target){
                 end = mid-1;
-             }
-             else if(mid>=0 && nums[mid]<target){
-                start = mid+1;
-             }
-             else if(mid>=0 && nums[mid]==target){
+            }
+            else{
                 i = mid;
-                j= mid;
-                start = -7899651;
-                end =  -11;
-             }
-             else if(i-1>=0 && nums[i-1]==target){
-                    i= i-1;
+                while(i>=0 && nums[i]==target){
+                    i--;
                 }
-             else{
-                
-                  if(j+1<nums.length && nums[j+1]==target){
-                    j= j+1;
+                i = i+1;
+                ans[0] = i;
+                while(i<nums.length && nums[i]==target){
+                    i++;
                 }
-                else if(i-1<0 || nums[i-1]!=target && j+1>=ans.length || nums[j+1]!=target){
-                    ans[0]= i;
-                    ans[1] = j;
-                   break;
-                }
-             }  
+                ans[1] = i-1;
+                break;
+            }
         }
-        // if(nums.length>0 && nums[0]==target){
-        //     ans[0] = 0;
-        // }
-        System.out.print(ans[0]);
         return ans;
     }
 }
