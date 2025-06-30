4800f358e919939867b5874e7a663a9a0472a8a6
Sagar97735 <sagarrajsingh370@gmail.com>
2025-06-30 09:47:34 +0530

    Time: 0 ms (100%), Space: 42.1 MB (67.74%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,39 +1,54 @@
 class Solution {
+     public int findMin(int[] nums) {
+        
+       int start = 0;
+       int end = nums.length-1;
+       int mid = 0;
+       while(start<end){
+          mid = (start + end)/2;
+          if(nums[mid]<nums[end]){
+            end = mid;
+          }
+          else if(nums[mid]>nums[end]){
+            start = mid+1;
+            mid = start;
+          }
+       }
+       return mid;
+    }
+
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            // Check if the target is at the mid index
-            if (nums[mid] == target) {
+        int ans = 0; 
+        int start = findMin(nums);
+        int end1 = nums.length-1;
+        int end = 0;
+        if(start == 0){
+            end = nums.length-1;
+        }
+        else if(nums[start]==target){
+            return start;
+        }
+        else if(nums[0] <= target){
+            end = start-1;
+            start = 0;
+        }
+        else if(nums[0] > target){
+            end = end1;
+        }
+        int mid = 0;
+         while(start<=end){
+            mid = (start + end)/2;
+            if(nums[mid]<target){
+               start = mid+1;
+            }
+            else if(nums[mid]>target){
+                end = mid-1;
+            }
+            else{
                 return mid;
             }
-
-            // Determine which part is sorted
-            if (nums[start] <= nums[mid]) {
-                // Left part is sorted
-                if (target >= nums[start] && target < nums[mid]) {
-                    // Target is in the left part
-                    end = mid - 1;
-                } else {
-                    // Target is in the right part
-                    start = mid + 1;
-                }
-            } else {
-                // Right part is sorted
-                if (target > nums[mid] && target <= nums[end]) {
-                    // Target is in the right part
-                    start = mid + 1;
-                } else {
-                    // Target is in the left part
-                    end = mid - 1;
-                }
-            }
-        }
-
-        // Target not found
-        return -1;
+            
+    }
+    return -1;
     }
 }
