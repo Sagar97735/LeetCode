4b1c686f96d6c9f10c1e715590b07f3f8009cdef
Sagar97735 <sagarrajsingh370@gmail.com>
2025-07-06 10:05:42 +0530

    Time: 140 ms (82.11%), Space: 74 MB (75.79%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
@@ -1,35 +1,43 @@
-import java.util.*;
-
 class FindSumPairs {
-    int[] nums1;
-    int[] nums2;
-    Map<Integer, Integer> freqMap;
-
+    int [] nums1;
+    int [] nums2;
+    Map<Integer,Integer> h1;
     public FindSumPairs(int[] nums1, int[] nums2) {
         this.nums1 = nums1;
         this.nums2 = nums2;
-        freqMap = new HashMap<>();
-        for (int num : nums2) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        h1 = new HashMap<>();
+        for(int i = 0; i<nums2.length; i++){
+            h1.put(nums2[i],h1.getOrDefault(nums2[i],0)+1);
         }
     }
-
+    
     public void add(int index, int val) {
-        int oldVal = nums2[index];
-        int newVal = oldVal + val;
-        nums2[index] = newVal;
-
-        freqMap.put(oldVal, freqMap.get(oldVal) - 1);
-        if (freqMap.get(oldVal) == 0) freqMap.remove(oldVal);
-        freqMap.put(newVal, freqMap.getOrDefault(newVal, 0) + 1);
+         int oldval = nums2[index];
+         int newval = nums2[index]+val;
+         nums2[index] = newval;
+         h1.put(newval,h1.getOrDefault(newval,0)+1);
+         h1.put(oldval,h1.get(oldval)-1);
+         if(h1.get(oldval)==0){
+            h1.remove(oldval);
+         }
     }
-
+    
     public int count(int tot) {
-        int res = 0;
-        for (int a : nums1) {
-            int target = tot - a;
-            res += freqMap.getOrDefault(target, 0);
+        int result = 0;
+        //nums1[i] +nums2[j]==tot then result+1
+        //nums2[j] = tot-nums1[i];
+        for(int i =0; i<nums1.length; i++){
+            int a = nums1[i];
+            int freq = h1.getOrDefault(tot-a,0);
+            result=result+freq;
         }
-        return res;
+        return result;
     }
 }
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs obj = new FindSumPairs(nums1, nums2);
+ * obj.add(index,val);
+ * int param_2 = obj.count(tot);
+ */
