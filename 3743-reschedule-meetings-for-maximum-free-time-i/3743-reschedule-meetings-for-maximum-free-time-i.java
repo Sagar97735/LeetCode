7a109df554e1cf88b2a80ec431db60e360f6fe8a
Sagar97735 <sagarrajsingh370@gmail.com>
2025-07-09 12:34:32 +0530

    Time: 1 ms (100%), Space: 63.2 MB (47.78%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
@@ -2,30 +2,31 @@ class Solution {
     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
         int n = startTime.length;
         if(k>=n){
-             int total = 0;
-             for(int i =0; i<n; i++){
-               total += (endTime[i]-startTime[i]);
-             }
-             return eventTime - total;
+            int total =0;
+            for(int i=0; i<n; i++){
+                total+=(endTime[i]-startTime[i]);
+            }
+            return eventTime-total;
         }
-        int [] gap = new int[n+1];
-        gap[0] = startTime[0];
-        for(int i =1; i<startTime.length; i++){
-            gap[i] = startTime[i] - endTime[i-1];
+        int [] freetime = new int[n+1];
+        freetime[0] = startTime[0];
+        for(int i=1;i<startTime.length; i++){
+            freetime[i] = startTime[i] - endTime[i-1];
         }
-        gap[n] = eventTime - endTime[n-1];
-        int currtime =0;
+        freetime[n] = eventTime - endTime[n-1];
+        int currtime = 0;
         int maxtime = 0;
         for(int i =0; i<k+1; i++){
-            currtime+=gap[i];
+            currtime += freetime[i];
+          
         }
         maxtime = currtime;
-        int i =0; 
+        int i =0;
         int j = k+1;
-        while(j<gap.length){
-            currtime-= gap[i];
-            currtime += gap[j];
-            if(maxtime<currtime){
+        while(j<freetime.length){
+            currtime-=freetime[i];
+            currtime+=freetime[j];
+            if(currtime>maxtime){
                 maxtime = currtime;
             }
             i++;
