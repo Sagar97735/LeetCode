bb0b4d0805a9831a59ae9abd6f8e8f784082b7b7
Sagar97735 <sagarrajsingh370@gmail.com>
2025-08-12 08:04:09 +0530

    Time: 13 ms (83.68%), Space: 42 MB (96.32%) - LeetHub

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfWays(int n, int x) {
+        final int MOD = 1_000_000_007;
+        // find max base such that base^x <= n
+        int maxBase = 1;
+        while (Math.pow(maxBase + 1, x) <= n) maxBase++;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int base = 1; base <= maxBase; base++) {
+            int val = (int) Math.pow(base, x);
+            for (int s = n; s >= val; s--) {
+                dp[s] = (int) ((dp[s] + dp[s - val]) % MOD);
+            }
+        }
+        return dp[n];
+    }
+}
