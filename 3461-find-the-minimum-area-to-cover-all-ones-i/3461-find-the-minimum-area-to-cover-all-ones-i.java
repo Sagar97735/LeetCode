2bb11fbdff595ac210b88f897a6567500b474ea5
Sagar97735 <sagarrajsingh370@gmail.com>
2025-08-22 11:11:49 +0530

    Update 3461-find-the-minimum-area-to-cover-all-ones-i.java

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.java
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.java
@@ -25,3 +25,34 @@ class Solution {
         return l * b;
     }
 }
+//-----------------------------------------------------------------------------------------C++---------------------------------------------------------------------------------------------------------------
+// class Solution {
+// public:
+//     int minimumArea(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+
+//         int minRow = m, maxRow = -1;
+//         int minCol = n, maxCol = -1;
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (grid[i][j] == 1) {
+//                     minRow = min(minRow, i);
+//                     maxRow = max(maxRow, i);
+//                     minCol = min(minCol, j);
+//                     maxCol = max(maxCol, j);
+//                 }
+//             }
+//         }
+
+//         // agar 1 hi nahi mila
+//         if (maxRow == -1) return 0;
+
+//         int l = maxRow - minRow + 1;
+//         int b = maxCol - minCol + 1;
+
+//         return l * b;
+//     }
+// };
+
