2ae4b72e67fc4e81c3217de751ef4853f8c7887f
Sagar97735 <sagarrajsingh370@gmail.com>
2025-08-24 08:06:32 +0530

    Update 1586-longest-subarray-of-1s-after-deleting-one-element.java

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -23,3 +23,31 @@ class Solution {
         return maxLen - 1;
     }
 }
+//---------------------------------------------------------------------------------------C++-------------------------------------------------------------------------------------------------------------------------------------
+// class Solution {
+// public:
+//     int longestSubarray(vector<int>& nums) {
+//         int left = 0, zeroCount = 0, maxLen = 0;
+
+//         for (int right = 0; right < nums.size(); right++) {
+//             if (nums[right] == 0) {
+//                 zeroCount++;
+//             }
+
+//             // agar 1 se zyada zero ho gaye to shrink karo
+//             while (zeroCount > 1) {
+//                 if (nums[left] == 0) {
+//                     zeroCount--;
+//                 }
+//                 left++;
+//             }
+
+//             // window ka length calculate
+//             maxLen = max(maxLen, right - left + 1);
+//         }
+
+//         // ek element delete karna mandatory hai
+//         return maxLen - 1;
+//     }
+// };
+
