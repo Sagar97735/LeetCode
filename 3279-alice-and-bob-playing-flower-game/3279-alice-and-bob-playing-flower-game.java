5a2008936f316c3fa58c5f8d9fd812b2d287c872
Sagar97735 <sagarrajsingh370@gmail.com>
2025-08-29 10:02:15 +0530

    Time: 0 ms (100%), Space: 40.9 MB (32.5%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
@@ -1,13 +1,10 @@
 class Solution {
     public long flowerGame(int n, int m) {
        long ans = 0;
-       long odd = m/2;;
-       if(m%2==0){
-        ans += (n*odd);
-       }
-       else{
-       ans+=(n*odd);
-       ans+=n/2;
+       long odd = m/2;
+       ans += (n*odd);
+       if(m%2!=0){
+        ans+=n/2;
        }
        return ans;
     }
