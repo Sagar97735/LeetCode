4448953ebf9df607e6d4338e4670512ca190fd15
Sagar97735 <sagarrajsingh370@gmail.com>
2025-08-30 12:08:08 +0530

    Time: 18 ms (5.43%), Space: 45.6 MB (5.08%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         HashSet<String> h1 = new HashSet<>();
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') {
                     String s = "(" + board[i][j] + ")";
                     if (!h1.add(s + i) || !h1.add(j + s) || !h1.add((i / 3) + s + (j / 3))) {
