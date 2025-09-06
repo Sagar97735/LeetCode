d51098632ec6f6ce6720d04724ba0253d80f4351
Sagar97735 <sagarrajsingh370@gmail.com>
2025-09-06 11:06:06 +0530

    Time: 15 ms (77.78%), Space: 101.5 MB (13.89%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.java
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.java
@@ -1,22 +1,31 @@
 class Solution {
     public long minOperations(int[][] queries) {
         long ans = 0;
-        for (int i = 0; i < queries.length; i++) {
-            long start = queries[i][0], end = queries[i][1];
-            long ops = 0;
-            for (long d = 1, prev = 1; d < 17; d++) {
-                long cur = prev * 4;
-                // Find the intersection between the query interval [start, end] and [prev, cur - 1]
-                long l = Math.max(start, prev);
-                long r = Math.min(end, cur - 1);
-                if (r >= l) {
-                    ops += (r - l + 1) * d;
-                }
-                prev = cur;
-            }
-            // Each operation handles 2 steps, so we take ceil(ops / 2)
-            ans += (ops + 1) / 2;
+        for(int i =0; i<queries.length; i++){
+            int start = queries[i][0];
+            int end = queries[i][1];
+            long steps = getOperations(start, end);
+            ans += ((steps+1)/2);
         }
         return ans;
+    }
+    public long getOperations(int start, int end){
+            long operations = 0;
+            long steps = 1;
+            long low = 1;
+            while(steps > 0){
+                if(end<low){
+                    break;
+                }
+                long high = 4*low -1;
+                long l = Math.max(start,low);
+                long r = Math.min(end,high);
+                if(l<=r){
+                    operations += (r-l+1)*steps;
+                }
+                steps++;
+                low = low*4;
+            }
+            return operations;
     }
 }
