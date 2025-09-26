39f0c99c2cbbe9707a40d1b7c7a9061f05be391a
Sagar97735 <sagarrajsingh370@gmail.com>
2025-09-26 08:42:05 +0530

    Time: 23 ms (85.21%), Space: 43.3 MB (83.53%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 class Solution {
     public int triangleNumber(int[] nums) {
-        Arrays.sort(nums); // sort the sides
+        Arrays.sort(nums); 
         int n = nums.length;
         int cnt = 0;
 
-        for (int k = n - 1; k >= 2; k--) {  // fix the largest side
+        for (int k = n - 1; k >= 2; k--) {  
             int i = 0, j = k - 1;
             while (i < j) {
                 if (nums[i] + nums[j] > nums[k]) {
-                    cnt += (j - i);  // all pairs between i..j-1 will also form triangle
+                    cnt += (j - i); 
                     j--;
                 } else {
                     i++;
