343bfc8743e03853f252bb0c5a6e66306c86c8a8
Sagar97735 <sagarrajsingh370@gmail.com>
2025-11-21 13:38:40 +0530

    Time: 2656 ms (5.16%), Space: 49.7 MB (6.69%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,52 +1,33 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int ans = 0;
-        
-        // Iterate through all 26 possible outer characters ('a' through 'z')
-        for (int i = 0; i < 26; i++) {
-            char c = (char) ('a' + i);
-            
-            // 1. Find the first occurrence (L)
-            int L = -1;
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == c) {
-                    L = j;
-                    break; // Found the first one
-                }
-            }
-            
-            // 2. Find the last occurrence (R)
-            int R = -1;
-            for (int j = s.length() - 1; j >= 0; j--) {
-                if (s.charAt(j) == c) {
-                    R = j;
-                    break; // Found the last one
-                }
-            }
-            
-            // 3. Check if the character appears at least twice (L < R)
-            if (L < R) {
-                // The palindromes are of the form 'c x c', where x is any character 
-                // between index L and index R (exclusive).
-                
-                // Use a boolean array to track the unique middle characters found
-                boolean[] middleChars = new boolean[26];
-                int uniqueMiddleCount = 0;
-                
-                // 4. Iterate through the substring s[L+1...R-1]
-                for (int j = L + 1; j < R; j++) {
-                    int charIndex = s.charAt(j) - 'a';
-                    if (!middleChars[charIndex]) {
-                        middleChars[charIndex] = true;
-                        uniqueMiddleCount++;
-                    }
-                }
-                
-                // 5. Add the number of unique middle characters to the total answer
-                ans += uniqueMiddleCount;
+        int [] left = new int[26];
+        int [] right = new int[26];
+        Arrays.fill(left,-1);
+        Arrays.fill(right,-1);
+        for(int i = 0; i<s.length(); i++){
+            int idx = s.charAt(i)-'a';
+            if(left[idx]==-1){
+                left[idx] = i;
             }
         }
-        
-        return ans;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int idx = s.charAt(i) - 'a';
+            if (right[idx] == -1) {
+                right[idx] = i;
+            }
+        }
+        HashSet<String> h1 = new HashSet<>();
+        for(int i =0; i<26; i++){
+            if(left[i]!=-1 && right[i]!=-1 && left[i]<right[i] && right[i]-left[i]+1>2){
+                for(int j = left[i]+1; j<right[i]; j++){
+                    String s1 = "";
+                    s1 += s.charAt(left[i]);
+                    s1+=s.charAt(j);
+                    s1 += s.charAt(left[i]);
+                    h1.add(s1);
+                }
+            }
+        }
+        return h1.size();
     }
 }
