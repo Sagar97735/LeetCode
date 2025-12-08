f7d331ca28cc6d116d60a422ada19ce8b0a2df7b
Sagar97735 <sagarrajsingh370@gmail.com>
2025-12-08 15:01:51 +0530

    Time: 57 ms (24.9%), Space: 99.4 MB (15.87%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,37 +1,32 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        HashSet<Integer> set = new HashSet<>();
-        long sum = 0, ans = 0;
-        int i = 0, j = 0;
-
+        long ans = 0;
+        long sum = 0;
+        HashMap<Integer,Integer> h1 = new HashMap<>();
+        for (int i = 0; i < k; i++) {
+            h1.put(nums[i],h1.getOrDefault(nums[i],0)+1);
+            sum += nums[i];
+        }
+        if (h1.size() == k) {
+            ans = Math.max(ans, sum);
+        }
+        int i = 0;
+        int j = k;
         while (j < nums.length) {
-
-            // Remove duplicates by shrinking from left
-            while (set.contains(nums[j])) {
-                set.remove(nums[i]);
-                sum -= nums[i];
-                i++;
-            }
-
-            // Now nums[j] is unique
-            set.add(nums[j]);
+            h1.put(nums[j],h1.getOrDefault(nums[j],0)+1);
+            h1.put(nums[i],h1.getOrDefault(nums[i],0)-1);
             sum += nums[j];
-
-            // If window size becomes > k → shrink
-            if (j - i + 1 > k) {
-                set.remove(nums[i]);
-                sum -= nums[i];
-                i++;
+            sum -= nums[i];
+            if(h1.get(nums[i])==0){
+                h1.remove(nums[i]);
             }
-
-            // When window size is exactly k → consider answer
-            if (j - i + 1 == k) {
+            if (j - i == k && h1.size() == k) {
                 ans = Math.max(ans, sum);
             }
-
             j++;
-        }
+            i++;
 
+        }
         return ans;
     }
 }
