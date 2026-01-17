2e5c79bc43027ee78b1d5260ec84fe67c6c96492
Sagar97735 <sagarrajsingh370@gmail.com>
2026-01-17 11:47:37 +0530

    Time: 96 ms (19.92%), Space: 83.9 MB (9.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,36 +1,29 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for(int i =0; i< nums.length; i++){
-            sum+=nums[i];
-        }
-        if(sum%2==1){
-            return false;
-        }
-        sum = sum/2;
-        int dp[][] = new int[nums.length+1][sum+1];
-        for(int i = 0; i<=nums.length; i++){
-           for(int j =0; j<sum+1;j++){
-            if(i==0||j==0){
-                dp[i][j]=0;
-            }
-           }
-        }
-        for(int i =1; i<nums.length+1;i++){
-            for(int j =1; j<sum+1; j++){
-              if(nums[i-1]<=j){//include
-                 int ans1 = nums[i-1]+dp[i-1][j-nums[i-1]];
-                 int ans2 = dp[i-1][j];
-                 dp[i][j] = Math.max(ans1,ans2);
-              }
-              else{
-                dp[i][j]=dp[i-1][j];
-              }
-            }
-        }
-        if(dp[nums.length][sum]== sum){
+    public boolean find(int [] nums,int req,int sum,int i, Boolean [][] dp){
+        if(sum==req){
             return true;
         }
-        return false;
+        if(i>=nums.length || sum>req){
+            return false;
+        }
+        if(dp[sum][i]!=null){
+            return dp[sum][i];
+        }
+        boolean b1 = find(nums,req,sum+nums[i],i+1,dp);
+        boolean b2 = find(nums,req,sum,i+1,dp);
+        return dp[sum][i] = b1 || b2;
+    }
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        
+        for(int i =0; i<nums.length; i++){
+          sum+=nums[i];
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        int req = sum/2;
+        Boolean [][] dp = new Boolean[req+1][nums.length+1];
+        return find(nums,req,0,0,dp);
     }
 }
