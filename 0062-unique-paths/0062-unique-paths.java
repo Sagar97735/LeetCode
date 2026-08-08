6de13b62ee985af0b3658b025eaba5a1283b0165
Sagar97735 <sagarrajsingh370@gmail.com>
2026-08-08 20:33:25 +0530

    Time: 0 ms (100%), Space: 42.1 MB (66.6%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,41 +1,20 @@
-//------------------------------>Recursive approach<---------------------------
-// class Solution {
-//     public void ispath(int m, int n,int i, int j,int[] arr){
-//       if(i==m-1 && j==n-1){
-//         arr[0] = arr[0]+1;
-//         return;
-//       }
-//       if(i+1<m){
-//          ispath(m,n,i+1,j,arr); 
-//       }
-//       if(j+1<n){
-//         ispath(m,n,i,j+1,arr);
-//       }
-//       return;
-//     }
-//     public int uniquePaths(int m, int n) {
-//     int [] arr = new int[1];
-//     ispath(m,n,0,0,arr);
-//     return arr[0];
-//     }
-// }
-public class Solution {
+class Solution {
+    public int solve(Integer [][] arr , int i, int j){
+       if(i>=arr.length || j>=arr[0].length){
+        return 0;
+       }
+       if(arr[i][j]!=null){
+        return arr[i][j];
+       }
+       if(i==arr.length-1 && j==arr[0].length-1){
+        return 1;
+       }
+      int c1 = solve(arr,i+1,j);
+      int c2 = solve(arr,i,j+1);
+      return arr[i][j] = c1+c2;
+    }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        
-        for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
-        }
-        
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        
-        return dp[m-1][n-1];
+        Integer [][] arr = new Integer[m][n];
+        return solve(arr,0,0);
     }
 }
