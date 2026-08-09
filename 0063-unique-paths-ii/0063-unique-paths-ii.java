821668c9a0caf491c6ad59a93ddfb404910a8ab1
Sagar97735 <sagarrajsingh370@gmail.com>
2026-08-09 10:16:05 +0530

    Time: 0 ms (100%), Space: 43.4 MB (81.81%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,53 +1,30 @@
-//-----------------------------recursive approach<-----------------------------
-// class Solution {
-//     public void ispath(int[][] obstacleGrid , int m, int n,int i, int j, int[] arr){
-//         if(i==m-1 && j==n-1){
-//             arr[0] = 1+arr[0];
-//             return;
-//         }
-//         if(i+1<m && obstacleGrid[i+1][j]==0){
-//             ispath(obstacleGrid,m,n,i+1,j,arr);
-//         }
-//         if(j+1<n && obstacleGrid[i][j+1]==0){
-//             ispath(obstacleGrid,m,n,i,j+1,arr);
-//         }
-//         return;
-//     }
-//     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-//         if(obstacleGrid[0][0]==1 || obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1]==1){
-//             return 0;
-//         }
-//         int [] arr = new int[1];
-//         int m = obstacleGrid.length;
-//         int n = obstacleGrid[0].length;
-//         ispath(obstacleGrid,m,n,0,0,arr);
-//         return arr[0];
-//     }
-// }
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        
-        // If starting or ending cell is blocked, no paths possible
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
-        
-        int[][] dp = new int[m][n];
-
-        // Initialize first cell
-        dp[0][0] = 1;
-
-        // Fill DP table
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;  // No paths possible through obstacles
-                } else {
-                    if (i > 0) dp[i][j] += dp[i - 1][j]; // From top
-                    if (j > 0) dp[i][j] += dp[i][j - 1]; // From left
-                }
-            }
+    public int solve(int[][] arr, int i, int j,Integer[][] dp){
+         if(i>=arr.length && j >= arr[0].length){
+            return 0;
         }
-        return dp[m - 1][n - 1]; // Bottom-right cell contains the answer
+        if(i==arr.length-1 && j ==arr[0].length-1 && arr[i][j]!=1){
+            return 1;
+        }
+        if(dp[i][j]!=null){
+            return dp[i][j];
+        }
+       
+        int c1 = 0;
+        int c2 = 0;
+        if(j+1<arr[0].length && arr[i][j+1]!=1){
+            c1 = solve(arr,i,j+1,dp);
+        }
+        if(i+1<arr.length && arr[i+1][j]!=1){
+            c2 = solve(arr,i+1,j,dp);
+        }
+        return dp[i][j] = c1+c2;
+    }
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid[0][0]==1){
+            return 0;
+        }
+        Integer[][] dp = new Integer[obstacleGrid.length+1][obstacleGrid[0].length+1];
+        return solve(obstacleGrid,0,0,dp);
     }
 }
