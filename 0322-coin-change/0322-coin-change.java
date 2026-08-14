92e9d16d4e0ff3b0d935b0df6aced0e059d858e7
Sagar97735 <sagarrajsingh370@gmail.com>
2026-08-14 15:41:56 +0530

    Time: 40 ms (7.58%), Space: 48.6 MB (5.54%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,35 +1,31 @@
 class Solution {
-    public void isSolve(int [] coins, int [][] dp){
+    public int solve(int[] coins, int amount,int i, Integer [][] dp ){
+         if(amount==0){
+        return 0;
+       }
+       if(i>=coins.length){
+        return Integer.MAX_VALUE-1000;
+       }
+       if(dp[i][amount]!=null){
+        return dp[i][amount];
+       }
+        int c1 = Integer.MAX_VALUE - 1000;
+        int c2 = Integer.MAX_VALUE - 1000;
+       if(amount>=coins[i]){
+        c1 = 1+solve(coins,amount-coins[i],i,dp);
+       }
+      
+         c2 = solve(coins,amount,i+1,dp);
         
-      for(int i =1; i<dp.length; i++){
-        for(int j =1; j<dp[0].length; j++){
-            if(coins[i-1]<=j){
-                      dp[i][j] = Math.min(dp[i-1][j],1+ dp[i][j-coins[i-1]]);
-                }
-            
-            else{
-                dp[i][j] = dp[i-1][j];
-            }
-        }
-      }
-      return;
+       return dp[i][amount] = Math.min(c1,c2);
     }
     public int coinChange(int[] coins, int amount) {
-        if(amount ==0){
-            return 0;
-        }
-        int n = coins.length;
-        int [][] dp = new int[coins.length+1][amount+1];
-        Arrays.sort(coins);
-        for (int i = 0; i <= coins.length; i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (j == 0) dp[i][j] = 0; // 0 coins to make 0 amount
-                else dp[i][j] = Integer.MAX_VALUE - 1;
-            }
-        }
+        Integer [][] dp = new Integer[coins.length+1][amount+1];
+         int ans = solve(coins, amount, 0,dp);
 
-       
-        isSolve(coins,dp);
-         return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
+        if (ans >= Integer.MAX_VALUE - 1000) {
+            return -1;
+        }
+        return ans;
     }
 }
