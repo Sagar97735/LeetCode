b0f4c76a2f290f91e4a181eb5c7dab4fafee0483
Sagar97735 <sagarrajsingh370@gmail.com>
2026-08-21 10:16:35 +0530

    Time: 5 ms (66.47%), Space: 47.1 MB (52.91%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,3 +1,42 @@
+// recursion based
+// class Solution {
+
+//     public int solve(String word1, String word2, int i, int j) {
+
+//         // word1 empty
+//         if(i == 0) {
+//             return j;
+//         }
+
+//         // word2 empty
+//         if(j == 0) {
+//             return i;
+//         }
+
+//         // characters same
+//         if(word1.charAt(i - 1) == word2.charAt(j - 1)) {
+//             return solve(word1, word2, i - 1, j - 1);
+//         }
+
+//         // replace
+//         int replace = solve(word1, word2, i - 1, j - 1);
+
+//         // delete
+//         int delete = solve(word1, word2, i - 1, j);
+
+//         // insert
+//         int insert = solve(word1, word2, i, j - 1);
+
+//         return 1 + Math.min(
+//             replace,
+//             Math.min(delete, insert)
+//         );
+//     }
+
+//     public int minDistance(String word1, String word2) {
+//         return solve(word1, word2, word1.length(), word2.length());
+//     }
+// }
 class Solution {
     public int minDistance(String word1, String word2) {
     int n = word1.length();
